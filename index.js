04b9db1625c3b18e9467def6e41981aed5939770
blakebyrnes <blakebyrnes@gmail.com>
2020-06-09 16:19:42 -0400

    Add “header-id” functionality to slug

--- index.js
+++ index.js
@@ -17,13 +17,40 @@ function transformer(ast) {
   visit(ast, 'heading', visitor)
 
   function visitor(node) {
+    checkForHeaderId(node);
     var data = node.data || (node.data = {})
     var props = data.hProperties || (data.hProperties = {})
     var id = props.id
+
+    console.log('building slug', id)
 
     id = id ? slugs.slug(id, true) : slugs.slug(toString(node))
 
     data.id = id
     props.id = id
+  }
+}
+
+function checkForHeaderId(node) {
+  let lastChild = node.children[node.children.length - 1]
+  if (lastChild && lastChild.type === 'text') {
+    let string = lastChild.value.replace(/ +$/, '')
+    let matched = string.match(/ {#([^]+?)}$/)
+
+    if (matched) {
+      let id = matched[1]
+      if (!!id.length) {
+        if (!node.data) {
+          node.data = {}
+        }
+        if (!node.data.hProperties) {
+          node.data.hProperties = {}
+        }
+        node.data.id = node.data.hProperties.id = id
+
+        string = string.substring(0, matched.index)
+        lastChild.value = string
+      }
+    }
   }
 }
